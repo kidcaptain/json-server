081ac315433e2f4fa6cad5ebf449a7536a066000
hunterjul007 <93106837+hunterjul007@users.noreply.github.com>
2024-06-19 03:54:25 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -10,6 +10,22 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
+const { parse } = require('@vue/compiler-sfc');
+
+const html = `
+  <div class="container">
+    <h1>This is a heading</h1>
+    <p>This is a paragraph</p>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  </div>
+`;
+
+const parsed = parse(html);
+console.log(parsed);
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is unning')
